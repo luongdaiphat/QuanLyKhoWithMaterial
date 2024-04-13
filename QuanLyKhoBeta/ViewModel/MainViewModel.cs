3ef7f8530ea0277991b41f23c1ba170f2197478c
Lường Đại Phát <21522443@gm.uit.edu.vn>
2024-04-14 00:00:14 +0700

    add base MVVM

---
+++ QuanLyKhoBeta/ViewModel/MainViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace QuanLyKhoBeta.ViewModel
+{
+    public class MainViewModel : BaseViewModel
+    {
+        // mọi thứ xử lý sẽ nằm trong này
+        public MainViewModel()
+        {
+            MessageBox.Show("Đã vào trong MainViewModel -> DataContext của mainwindow.xaml");
+        }
+    }
+}
